9ea0db69e73365aa24dc4611cb33aa4bbdf1bdd8
Nixon <nixon@gmail.com>
2016-08-23 12:23:14 +0200

    Adicionando o método de teste que permite reservar todos ingressos disponíveis

--- src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
@@ -38,4 +38,12 @@ public class SessaoTest {
 		Assert.assertEquals(2, sessao.getIngressosDisponiveis().intValue());
 	}
 	
+	@Test
+	public void deveReservarTotalDeIngressosDisponiveis() throws Exception {
+		Sessao sessao = new Sessao();
+		sessao.setTotalIngressos(5);
+
+		Assert.assertTrue(sessao.podeReservar(5));
+	}
+	
 }
