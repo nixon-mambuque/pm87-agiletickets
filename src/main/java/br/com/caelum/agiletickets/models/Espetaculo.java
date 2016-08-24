842d62c4e9e487ca398b783e821c62766cba7520
Nixon <nixon@gmail.com>
2016-08-24 10:10:03 +0200

    adicionamos a lógica para criar uma sessão se a dataInicío = dataFim

--- src/main/java/br/com/caelum/agiletickets/models/Espetaculo.java
+++ src/main/java/br/com/caelum/agiletickets/models/Espetaculo.java
@@ -2,6 +2,7 @@ package br.com.caelum.agiletickets.models;
 
 import static com.google.common.collect.Lists.newArrayList;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Entity;
@@ -99,8 +100,15 @@ public class Espetaculo {
 	public List<Sessao> criaSessoes(LocalDate inicio, LocalDate fim, LocalTime horario, Periodicidade periodicidade) {
 		// ALUNO: Não apague esse metodo. Esse sim será usado no futuro! ;)
 		
+		if(inicio.equals(fim)){
+			Sessao e =new Sessao();
+			List<Sessao> sessoes = new ArrayList<Sessao>();
+			sessoes.add(e);
+			return sessoes;
+		} 
+		else
+		
 		throw new IllegalArgumentException();
-		//return null;
 	}
 	
 	public boolean Vagas(int qtd, int min)
